768bf59f3c3ab27cf775ae8b4fee465b74267a2d
Bronley <bronley@gmail.com>
2021-05-28 15:12:46 -0400

    Initial setup script add.

---
+++ README.md
@@ -0,0 +1,7 @@
+# pi-wifi
+This is a Raspberry PI project that scans nearby wifi access points, and regularly picks the strongest signal. I created this to solve the very specific purpose of getting more stable wifi in the campground where we vacation every summer.
+
+## Setup
+1. install RaspberryPI OS on your raspberry pi
+2. Run the setup.sh script from this repository
+3. Once connected to `pi-wifi`, navigate to `http://192.168.10.1` and configure the RaspberryPI through the pi-wifi admin console.
---
+++ setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
+password=romantic
+
+
+#change the default password
+echo -e "$password\n$password" | passwd pi
+
+#enable SSH service on startup. 0 means enabled
+raspi-config nonint do_ssh 0
+
+#tell the PI what your wifi country code is
+raspi-config nonint do_wifi_country US
+
+# expand the root filesystem to fill SD card. This allows you to use your entire SD card
+raspi-config nonint do_expand_rootfs
+
+#change the device hostname
+printf "pi-wifi" > /etc/hostname
+sed -i "s/raspberrypi/pi-wifi/g" /etc/hosts
+
+# install all of the latest updates
+apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y && apt full-upgrade -y
+
+#force the onboard wireless to be identified as wlan2 (so we can dependably use wlan0 and wlan1 for the usb dongles)
+cat > /etc/udev/rules.d/72-static-name.rules <<EOF
+ACTION=="add", SUBSYSTEM=="net", DRIVERS=="brcmfmac", NAME="wlan2"
+EOF
+
+#Installing samba to use for network shares
+apt-get install samba samba-common-bin cifs-utils -y
+
+#Creating common samba shares
+printf "[global]
+workgroup=Workgroup
+netbios name = pi-wifi
+
+[root]
+    comment = root
+    path=/
+    browseable=YES
+    writeable=YES
+    valid users= pi, root
+    only guest=no
+    create mask=0777
+    directory mask=07777
+    public=no
+    force user = root
+
+" > /etc/samba/smb.conf
+
+#set the samba passwords
+(echo $password; echo $password) | smbpasswd -s -a root
+(echo $password; echo $password) | smbpasswd -s -a pi
+
+#restart the samba service so it picks up the new shares
+service smbd restart
+service nmbd restart
+
+#install fail2ban to prevent brute force ssh attacks
+apt-get install fail2ban -y
+
+printf "
+[DEFAULT]
+bantime = 3600
+maxretry = 5
+
+[sshd]
+enabled = true
+" > /etc/fail2ban/jail.local
+
+service fail2ban restart
+
+#download the pi-wifi project
+#TODO
